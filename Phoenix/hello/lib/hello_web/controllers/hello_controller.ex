88da434c4034a05face7df13d0b9b2a246dd8f4f
Chris Barfod <chrisbarfod@Chriss-MacBook-Pro.local>
2018-11-29 17:08:24 +1100

    routing

--- Phoenix/hello/lib/hello_web/controllers/hello_controller.ex
+++ Phoenix/hello/lib/hello_web/controllers/hello_controller.ex
@@ -6,5 +6,12 @@ defmodule HelloWeb.HelloController do
     # Phoenix will look for the template in a dir named after our controller
     render(conn, "index.html")
   end
+
+  # Pattern match against the params passed into the show func so that 'messenger'
+  # var is bound to value put in the :messenger position in URL
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, "show.html", messenger: messenger)
+  end
+ 
 end
 
